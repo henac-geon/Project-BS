e4f37d91a46aae0efa2bd0e4795e6beb5e61111e
henac <man25816@naver.com>
2025-04-30 17:47:42 +0900

    파일 밑 폴더 제작 2차

---
+++ Project-BS/FantasyBook.cpp
@@ -0,0 +1,14 @@
+#include "FantasyBook.h"
+
+FantasyBook::FantasyBook(const std::string& title, const std::string& description, int length)
+    : Book(title, description, eBookGenre::Fantasy, eBookMood::Whimsical, length) {
+}
+
+void FantasyBook::repair() {
+    condition = eBookCondition::Perfect;
+    isDamaged = false;
+}
+
+void FantasyBook::displayInfo() const {
+    std::cout << "Fantasy Book: " << title << " - " << description << " (" << length << " pages)" << std::endl;
+}
