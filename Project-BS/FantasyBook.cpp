0c75bf4ce4affe566bc3071705137a6910b07452
태윤 박 <qkrxodbs05@gmail.com>
2025-05-06 20:15:26 +0900

    책 속성추가 책 제작기능 구현중.

--- Project-BS/FantasyBook.cpp
+++ Project-BS/FantasyBook.cpp
@@ -1,5 +1,7 @@
 #include "FantasyBook.h"
 
+
+//장르까진 그러려니 하는데 무드나 딴거는 어떻게 떙겨올지 생각해봐야함.
 FantasyBook::FantasyBook(const std::string& title, const std::string& description, int length)
     : Book(title, description, eBookGenre::Fantasy, eBookMood::Whimsical, length, eBookEdge::None, eBookEtc::None) {
 }
